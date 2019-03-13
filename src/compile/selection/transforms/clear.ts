4f9502a18d6c64da36549d579315d82e3add5c54
Allen Lee <allenlee@mit.edu>
2019-03-13 01:11:33 -0400

    updating tx type to modify existing signal

--- src/compile/selection/transforms/clear.ts
+++ src/compile/selection/transforms/clear.ts
@@ -1,22 +1,18 @@
 import {TransformCompiler} from './transforms';
-
-const CLEAR = '_clear';
-
-const CLEAR_EVENTS = {click: 'dblclick', mouseover: 'mouseout'};
+import {TUPLE} from '..';
 
 const clear: TransformCompiler = {
   has: selCmpt => {
-    return true;
+    return selCmpt.type === 'multi' && selCmpt.clear !== 'none';
   },
 
   signals: (model, selCmpt, signals) => {
-    const type = CLEAR_EVENTS[selCmpt.events[0].type];
-
-    return signals.concat({
-      name: selCmpt.name + CLEAR,
-      value: false,
-      on: [{events: [{source: 'scope', type: type}], update: `modify(\"${selCmpt.name}_store\", null, true)`}]
+    const tupleIdx = signals.findIndex(i => i.name === selCmpt.name + TUPLE);
+    signals[tupleIdx].on.push({
+      events: selCmpt.clear,
+      update: 'null'
     });
+    return signals;
   }
 };
 
